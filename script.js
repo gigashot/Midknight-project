0158a5fa34a4dca86c8a771cc03ae6451031723c
marek eichert <eic42401@spseol.cz>
2024-06-02 21:48:47 +0200

    paralax effect added

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-window.addEventListener('scroll', function() {
-    var navbar = document.getElementById('navbar');
-    if (window.scrollY > 100) {
-        navbar.style.backgroundColor = 'rgba(0, 0, 0, 0.8)'; // Change the background color when scrolled
-    } else {
-        navbar.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
-    }
-});
+// window.addEventListener('scroll', function() {
+//     var navbar = document.getElementById('navbar');
+//     if (window.scrollY > 100) {
+//         navbar.style.backgroundColor = 'rgba(0, 0, 0, 0.8)'; // Change the background color when scrolled
+//     } else {
+//         navbar.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+//     }
+// });
